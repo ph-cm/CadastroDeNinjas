6a77041cf949bee615213f9eeea91323f46df30d
Pedro Henrique <trabalho.prog.pedro@gmail.com>
2025-08-24 01:06:29 -0300

    Primeira rota usando controller

---
+++ src/main/java/dev/balastro/cadastrodeninjas/Controller.java
@@ -0,0 +1,17 @@
+package dev.balastro.cadastrodeninjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //falando o que é uma annotation controller (Fala que é um controller)
+@RequestMapping // mapeamento de rotas(colocar todas as rotas num mesmo lugar
+public class Controller {
+    // Controller
+
+    @GetMapping("/boasvindas") // pegar info boasvindas e joga na rota localhost:8080/boasVindas
+    public String boasVindas(){
+        return "Essa é minha primeira mensagem nessa rota";
+    }
+
+}
