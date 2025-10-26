18f93d4416cfed0e2a38a3c5956c1028d18486b7
Pedro Henrique <trabalho.prog.pedro@gmail.com>
2025-10-26 17:38:27 -0300

    Atualiza o controller com o banco de dados

--- src/main/java/dev/balastro/cadastrodeninjas/Ninjas/Controller/NinjaController.java
+++ src/main/java/dev/balastro/cadastrodeninjas/Ninjas/Controller/NinjaController.java
@@ -1,8 +1,6 @@
 package dev.balastro.cadastrodeninjas.Ninjas.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController //falando o que é uma annotation controller (Fala que é um controller)
 @RequestMapping // mapeamento de rotas(colocar todas as rotas num mesmo lugar
@@ -13,4 +11,34 @@ public class NinjaController {
         return "Essa é minha primeira mensagem nessa rota";
     }
 
+    //Endpoints
+    //Adicionar Ninja (CREATE)
+    @PostMapping("/criar")
+    public String criarNinja(){
+        return "Ninja Criado";
+    }
+
+    //Procurar Ninja por ID (READ)
+    @GetMapping("/todos")
+    public String mostrarNinjaPorId(){
+        return "Ninja Tal de Id Tal";
+    }
+
+    //Mostrar Ninja (READ)
+    @GetMapping("/mostar")
+    public String mostrarTodosOsNinjas(){
+        return "Todos os Ninjas";
+    }
+
+    //Alterar Ninja(UPDATE)
+    @PutMapping("/alterarId")
+    public String alterarNinjaPorId(){
+        return "Ninja Alterado";
+    }
+
+    //Deletar Ninja(DELETE)
+    @DeleteMapping("/deletar")
+    public String deletarNinja(){
+        return "Ninja Deletado";
+    }
 }
