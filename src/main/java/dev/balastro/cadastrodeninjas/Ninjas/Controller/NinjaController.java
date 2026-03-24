79a6cb4d2d440039af23e35d1c5f2f329d7ba5c7
Pedro Henrique <trabalho.prog.pedro@gmail.com>
2026-03-24 15:42:57 -0300

    fix: ajustando o nome das rotas do ninja controller

--- src/main/java/dev/balastro/cadastrodeninjas/Ninjas/Controller/NinjaController.java
+++ src/main/java/dev/balastro/cadastrodeninjas/Ninjas/Controller/NinjaController.java
@@ -3,7 +3,7 @@ package dev.balastro.cadastrodeninjas.Ninjas.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController //falando o que é uma annotation controller (Fala que é um controller)
-@RequestMapping // mapeamento de rotas(colocar todas as rotas num mesmo lugar
+@RequestMapping("/ninjas") // mapeamento de rotas(colocar todas as rotas num mesmo lugar
 public class NinjaController {
     // Controller
     @GetMapping("/boasvindas") // pegar info boasvindas e joga na rota localhost:8080/boasVindas
