51a342d136d7eb770ea4dd402a74b6d08a1c4fb8
Pedro Henrique <trabalho.prog.pedro@gmail.com>
2026-03-24 15:40:22 -0300

    feat: Criar rtas do  missao controller com mock

--- src/main/java/dev/balastro/cadastrodeninjas/Missoes/Controller/MissaoController.java
+++ src/main/java/dev/balastro/cadastrodeninjas/Missoes/Controller/MissaoController.java
@@ -1,15 +1,42 @@
 package dev.balastro.cadastrodeninjas.Missoes.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
+import org.springframework.web.bind.annotation.*;
+
+//LOCALHOST:8080 == RPECISAMOS DE DESENVOOLVER AS ROTAS PARA O SERVIDOR "LOCALHOST:8080"
+//CAMADA MAIS PROXIMA DO USUARIO
+
+//ANOTATIONS
 @RestController // dizer que é um controller
-@RequestMapping // dizer que tera requisição de mapeamento
+@RequestMapping("missoes")// dizer que tera requisição de mapeamento - REDIRECIONAMENTO BASEADO EM PATH(CAMINHO)
 public class MissaoController {
 
-    @GetMapping("/boasvindasmissao")
-    public String boasVindas(){
+    @GetMapping("/saudacao")
+    public String saudarMissao() {
+        return "Bem vindo a aba de missoes";
+    }
+
+    //GET -- MANDAR REQUISICAO PRA MOSTRAR AS MISSOES
+    @GetMapping("/listar")
+    public String listarMissoes(){
+        return "Missoes listadas";
+    }
+
+    //POST -- MANDAR UMA REQUISICAO PARA CRIAR REQUISICOES
+    @PostMapping("/criar") //POST = ACESSO A ROTA - USUARIO MANDA PRA MIM
+    public String criarMissao(){
         return "Boas vindas a rota de missoes";
+    }
+
+    //PUT -- MANDAR REQUISICAO PARA ALTERAR AS MISSOES
+    @PutMapping("/alterar")
+    public String alterarMissao(){
+        return "Missao alterada com sucesso";
+    }
+
+    //DELETE -- REQUISICAO PARA DELETAR AS MISSOES
+    @DeleteMapping("/deletar")
+    public String deletarMissao(){
+        return "Missao deletada com sucesso";
     }
 }
